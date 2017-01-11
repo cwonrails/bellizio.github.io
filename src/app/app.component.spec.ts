cca18076492ebd1fc14a4d58f3968a003e7370f9
Devon Bellizio <jdbellizio@gmail.com>
2017-01-11 15:53:54 -0500

    fix app.component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,22 @@
 import { TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './_global/header/header.component';
+import { FooterComponent } from './_global/footer/footer.component';
 
 describe('App', () => {
+
   beforeEach(() => {
-    TestBed.configureTestingModule({ declarations: [AppComponent]});
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
+    });
   });
+
   it ('should work', () => {
     let fixture = TestBed.createComponent(AppComponent);
     expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
