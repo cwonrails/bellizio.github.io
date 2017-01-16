3aed3310c99d5db1a72579eace0b1f6afcbac430
Devon Bellizio <jdbellizio@gmail.com>
2017-01-16 15:14:22 -0500

    remove header and footer imports since NO_ERRORS_SCHEMA is used

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './_global/header/header.component';
-import { FooterComponent } from './_global/footer/footer.component';
 
 describe('App', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent,
-        HeaderComponent,
-        FooterComponent
+        AppComponent
       ],
       schemas: [ NO_ERRORS_SCHEMA ]
     });
